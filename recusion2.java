5c224950dfad4054a75a6440172af86c9f58c612
Niraj <155253763+nirajkr2707@users.noreply.github.com>
2024-08-02 18:03:19 +0530

    Create recusion2.java

---
+++ recusion2.java
@@ -0,0 +1,16 @@
+public class recu2 {
+
+    public static void printinc(int n) {
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        printinc(n-1);
+        System.out.println(n + " ");
+    }
+    public static void main(String[] args) {
+        int n=10;
+        printinc(n);
+    }
+}
+ 
