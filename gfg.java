51fbcd81bbbe9c359e0ffa2efec8d1bd823c58c7
Niraj <155253763+Nicode2707@users.noreply.github.com>
2024-10-25 20:21:05 +0530

    Create gfg.java

---
+++ gfg.java
@@ -0,0 +1,15 @@
+  vector<int>ans;
+        sort(arr.begin(),arr.end());
+        int i=0,j=arr.size()-1;
+        while(i<j){
+            ans.push_back(arr[j]);
+            ans.push_back(arr[i]);
+            i++;
+            j--;
+        }
+        if(i==j){
+            ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
