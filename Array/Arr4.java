62177773328bc3ab0b154ab05876b3f605d984c6
Niraj <155253763+Nicode2707@users.noreply.github.com>
2024-11-01 12:53:59 +0530

    Create Arr4.java

---
+++ Array/Arr4.java
@@ -0,0 +1,21 @@
+public class Arr4 {
+  //Reverse of Array 
+    public static void reverse(int arr[]) {
+        int start =0 ,last =arr.length-1;
+        while (start<last) {
+            int temp = arr[last];
+             arr[last] = arr[start];
+             arr[start] =temp;
+             
+             start++;
+             last--;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10,12};
+        reverse(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
