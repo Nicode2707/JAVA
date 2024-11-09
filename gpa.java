af2240d728cf72adce4e7fac67d8e24d0b0f374c
Niraj <155253763+Nicode2707@users.noreply.github.com>
2024-11-09 18:49:20 +0530

    Add files via upload

---
+++ gpa.java
@@ -0,0 +1,9 @@
+public class gpa {
+    public static void main(String[] args) {
+        int phy =64;
+        int che =59;
+        int maths = 87;
+        int calculate = phy + che +maths;
+        System.out.print(calculate);
+    }
+}
