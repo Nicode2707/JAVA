2cd71bfea44e3c0be88cd7d2f4192d2aa9cfb300
Niraj <155253763+Nicode2707@users.noreply.github.com>
2024-11-21 18:15:58 +0530

    Add files via upload

---
+++ product.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class product {
+    public static void main(String[] args) {
+
+       Scanner Sc = new Scanner(System.in);
+      
+       int number1 = Sc.nextInt();
+
+       int number2 = Sc.nextInt();
+
+       int product =number1*number2;
+
+       System.out.println(product);
+
+    }
+}
+
