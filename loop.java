d92f43a1e8e853bd5dfcd7d10e1ab8b88636f30d
Niraj <155253763+Nicode2707@users.noreply.github.com>
2024-11-22 14:21:50 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class loop {
+    public static void main(String[] args) {
+        int counter = 0 ;
+        while (counter < 100) {
+            System.out.println("HEY NIRAJ");
+            counter++;
+        } 
+    }
+    
+}
