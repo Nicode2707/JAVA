8d5156b7f937e4b7deb52b401034ca3feeb952c6
Niraj <155253763+Nicode2707@users.noreply.github.com>
2024-11-23 11:21:11 +0530

    Add files via upload

---
+++ mult.java
@@ -0,0 +1,11 @@
+public class mult {
+
+    public static int multi(int a,int b){
+       int multi =a*b;
+       System.out.println(multi);
+       return multi;
+    }
+    public static void main(String[] args) {
+        multi(3)
+    }
+}
