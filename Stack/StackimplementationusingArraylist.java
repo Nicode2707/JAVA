f97e5134a3fdcb1047ab72425b70dc45356efb62
Niraj <155253763+Nicode2707@users.noreply.github.com>
2025-01-20 19:49:56 +0530

    Create StackimplementationusingArraylist.java

---
+++ Stack/StackimplementationusingArraylist.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class S1{
+
+    static class stack{
+        static ArrayList<Integer> list = new  ArrayList<>();
+        public static boolean isEmpty() {
+          return list.size() == 0;
+        }
+        //push 
+        public static void push(int data){
+            list.add(data);
+        }
+        //pop
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top; 
+        }
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = list.get(list.size()-1);
+            return top; 
+        }
+    } 
+   
+
+
+    public static void main(String[] args) {
+        stack s = new stack();
+        s.push(1);
+        s.push(3);
+        s.push(5);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
